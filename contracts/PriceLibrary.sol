a0698f386603eaf9e9e1c8e71e606cf59d512a96
Anh Ngo <anhnq82@gmail.com>
2022-10-27 00:54:53 +0700

    Change access modifier

--- contracts/PriceLibrary.sol
+++ contracts/PriceLibrary.sol
@@ -12,18 +12,18 @@ library PriceLibrary {
     using FixedPoint for FixedPoint.uq112x112;
     using PriceLibrary for OraclePrice;
 
-    function isEmpty(OraclePrice memory self) public pure returns (bool) {
+    function isEmpty(OraclePrice memory self) internal pure returns (bool) {
         return self.base._x == 0;
     }
 
-    function muluq(OraclePrice memory self, FixedPoint.uq112x112 memory scale) public pure returns (OraclePrice memory) {
+    function muluq(OraclePrice memory self, FixedPoint.uq112x112 memory scale) internal pure returns (OraclePrice memory) {
         return OraclePrice(
             self.base.muluq(scale),
             self.LP.muluq(scale.sqrt())
         );
     }
 
-    function divuq(OraclePrice memory self, FixedPoint.uq112x112 memory scale) public pure returns (OraclePrice memory) {
+    function divuq(OraclePrice memory self, FixedPoint.uq112x112 memory scale) internal pure returns (OraclePrice memory) {
         return OraclePrice(
             self.base.divuq(scale),
             self.LP.divuq(scale.sqrt())
